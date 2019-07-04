a7e4e6ccfeab35053d8da2291d345734f5d6ecff
Bo Yavorskyi <bogdjavor@gmail.com>
2019-07-05 00:15:21 +0300

    Try-catch fix

--- src/main/java/CashCount.java
+++ src/main/java/CashCount.java
@@ -9,36 +9,47 @@ public class CashCount {
     private List<Integer> notes = Arrays.asList(1, 2, 5, 10, 20, 50, 100, 200, 500);
     private int[] noteNumber = new int[9];
 
+
     public void CashToNotes() {
-        System.out.print("Enter the sum of money: ");
-        int sum = scan.nextInt();
-        int valueToCountTime = sum;
-        long start = System.nanoTime();
-
-        if (sum > 0) {
-            for (int i = notes.size() - 1; i >= 0; i--) {
-                int notesAmount = sum / notes.get(i);
-                if (notesAmount >= 1) {
-                    sum -= notes.get(i) * notesAmount;
-                    noteNumber[i] = notesAmount;
-                }
-                notes.get(i);
-                int w = noteNumber[i];
-                if (w > 0) {
-                    System.out.println("\t" + notes.get(i) + " x " + noteNumber[i]);
-                }
+        int sum = 0;
+        boolean valid = false;
+        while (!valid) {
+            try {
+                System.out.print("Enter the value: ");
+                sum = scan.nextInt();
+                valid = true;
+            } catch (Exception e) {
+                System.out.println("Invalid input. Numbers only!");
+                scan.nextLine();
             }
-            long end = System.nanoTime();
-            double total = (double) (end - start) / 1000000;
+        }
 
-            if ((valueToCountTime >= 24565) && (valueToCountTime <= 35789)) {
-                System.out.println("Time spent - " + total + " (ms)");
+            int valueToCountTime = sum;
+            long start = System.nanoTime();
+            if (sum > 0) {
+                for (int i = notes.size() - 1; i >= 0; i--) {
+                    int notesAmount = sum / notes.get(i);
+                    if (notesAmount >= 1) {
+                        sum -= notes.get(i) * notesAmount;
+                        noteNumber[i] = notesAmount;
+                    }
+                    notes.get(i);
+                    int w = noteNumber[i];
+                    if (w > 0) {
+                        System.out.println("\t" + notes.get(i) + " x " + noteNumber[i]);
+                    }
+                }
+                long end = System.nanoTime();
+                double total = (double) (end - start) / 1000000;
+
+                if ((valueToCountTime >= 24565) && (valueToCountTime <= 35789)) {
+                    System.out.println("Time spent - " + total + " (ms)");
+                } else {
+                    System.exit(0);
+                }
             } else {
-                System.exit(0);
+                System.out.println("Invalid number, please enter again");
+                CashToNotes();
             }
-        } else {
-            System.out.println("Invalid number, please enter again");
-            CashToNotes();
         }
     }
-}
