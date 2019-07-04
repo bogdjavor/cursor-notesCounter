2ae61d9212b0421cb598e698c023364d34fad22d
Bo Yavorskyi <bogdjavor@gmail.com>
2019-07-04 20:06:06 +0300

    Fixed version

---
+++ src/com/java/CashCount.java
@@ -0,0 +1,44 @@
+package com.java;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CashCount {
+    private Scanner scan = new Scanner(System.in);
+    private List<Integer> notes = Arrays.asList(1, 2, 5, 10, 20, 50, 100, 200, 500);
+    private int[] noteNumber = new int[9];
+
+    public void CashToNotes() {
+        System.out.print("Enter the sum of money: ");
+        int sum = scan.nextInt();
+        int valueToCountTime = sum;
+        long start = System.nanoTime();
+
+        if (sum > 0) {
+            for (int i = notes.size() - 1; i >= 0; i--) {
+                int notesAmount = sum / notes.get(i);
+                if (notesAmount >= 1) {
+                    sum -= notes.get(i) * notesAmount;
+                    noteNumber[i] = notesAmount;
+                }
+                notes.get(i);
+                int w = noteNumber[i];
+                if (w > 0) {
+                    System.out.println("\t" + notes.get(i) + " x " + noteNumber[i]);
+                }
+            }
+            long end = System.nanoTime();
+            double total = (double) (end - start) / 1000000;
+
+            if ((valueToCountTime >= 24565) && (valueToCountTime <= 35789)) {
+                System.out.println("Time spent - " + total + " (ms)");
+            } else {
+                System.exit(0);
+            }
+        } else {
+            System.out.println("Invalid number, please enter again");
+            CashToNotes();
+        }
+    }
+}
--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,4 +1,4 @@
-package com.Homework;
+package com.java;
 
 public class Main {
     public static void main(String[] args) {
